aed2897a9ee6890a3b9203e6bc07419d516c0631
Matt Busche <mrbusche@gmail.com>
2017-03-06 22:00:46 -0600

    install service worker

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+(function() {
+	'use strict';
+
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker
+			.register('./service-worker.js')
+			.then(function() {
+				console.log('Service Worker Registered');
+			});
+	} else {
+		console.log('Service Worker Not Supported');
+	}
+})();
