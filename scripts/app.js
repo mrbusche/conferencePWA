fefed4d72286fc0a919b93f33ec1bc29a0e38911
Matt Busche <mrbusche@gmail.com>
2017-04-09 21:46:11 -0500

    get data from cache and url, move table building into separate function

--- scripts/app.js
+++ scripts/app.js
@@ -5,45 +5,48 @@
 
 	app.getConferenceData = function(key, label) {
 		var url = 'http://127.0.0.1:8080/conference.json';
+		if ('caches' in window) {
+			caches.match(url).then(function(response) {
+				if (response) {
+					response.json().then(function updateFromCache(json) {
+						console.log('from cache');
+						app.updateConfSchedule(json);
+					});
+				}
+			});
+		}
 		// Fetch the latest data.
 		var request = new XMLHttpRequest();
 		request.onreadystatechange = function() {
 			if (request.readyState === XMLHttpRequest.DONE) {
 				if (request.status === 200) {
-					//LASTUPDATE (new Date()).toJSON()
-					var response = JSON.parse(request.response);
-					console.log(response);
-					console.log(response.LASTUPDATE);
-					var sessions = response.SESSIONS;
-					console.log(sessions);
-					for (var session in sessions) {
-						var oneSession = sessions[session];
-						var tableRef = document.getElementById('schedule').getElementsByTagName('tbody')[0];
-						var newRow = tableRef.insertRow(0);
-						var newCell = newRow.insertCell(0);
-						newCell.appendChild(document.createTextNode(oneSession.TITLE));
-						newCell = newRow.insertCell(1);
-						newCell.appendChild(document.createTextNode(oneSession.SPEAKER));
-						newCell = newRow.insertCell(2);
-						newCell.appendChild(document.createTextNode(oneSession.STARTTIME));
-						newCell = newRow.insertCell(3);
-						newCell.appendChild(document.createTextNode(oneSession.ENDTIME));
-						tableRef.insertRow(tableRef.rows.length);
-
-						// console.log(oneSession);
-						// console.log(oneSession.STARTTIME);
-						// console.log(oneSession.ENDTIME);
-						// console.log(oneSession.TITLE);
-						// console.log(oneSession.SPEAKER);
-						// console.log(oneSession.DESCRIPTION);
-					}
-					console.log(tableRef.rows.length);
-					//app.updateConfSchedule(SESSIONS);
+					console.log('from request');
+					console.log(request);
+					app.updateConfSchedule(request.response);
 				}
 			}
 		};
 		request.open('GET', url);
 		request.send();
+	};
+
+	app.updateConfSchedule = function(data) {
+		console.log('update conf schedule');
+		var sessions = data.SESSIONS;
+		for (var session in sessions) {
+			var oneSession = sessions[session];
+			var tableRef = document.getElementById('schedule').getElementsByTagName('tbody')[0];
+			var newRow = tableRef.insertRow(0);
+			var newCell = newRow.insertCell(0);
+			newCell.appendChild(document.createTextNode(oneSession.TITLE));
+			newCell = newRow.insertCell(1);
+			newCell.appendChild(document.createTextNode(oneSession.SPEAKER));
+			newCell = newRow.insertCell(2);
+			newCell.appendChild(document.createTextNode(oneSession.STARTTIME));
+			newCell = newRow.insertCell(3);
+			newCell.appendChild(document.createTextNode(oneSession.ENDTIME));
+			tableRef.insertRow(tableRef.rows.length);
+		}
 	};
 
 	if ('serviceWorker' in navigator) {
